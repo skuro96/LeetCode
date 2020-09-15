748b3e6854f897569b329b873bee547a6543f173
skurosu <xsho0212@outlook.com>
2020-09-16 03:23:13 +0900

    Create 0007

---
+++ Problems/0007_ReverseInteger.py
@@ -0,0 +1,19 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        if x < 0:
+            sign = -1
+            x *= -1
+        else:
+            sign = 1
+        
+        ans = 0
+        place = 1
+        while x > place - 1:
+            ans *= 10
+            ans += int(int(x % (place * 10)) / place)
+            place *= 10
+
+        if ans * sign < -2**31 or 2**31 - 1 < ans * sign:
+            return 0
+            
+        return ans * sign
