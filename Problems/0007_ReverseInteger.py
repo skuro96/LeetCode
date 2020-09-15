3632edd381a86d2868617ee57507ab895f3b3d10
skurosu <xsho0212@outlook.com>
2020-09-16 03:26:28 +0900

    Create 0007

--- Problems/0007_ReverseInteger.py
+++ Problems/0007_ReverseInteger.py
@@ -8,12 +8,12 @@ class Solution:
         
         ans = 0
         place = 1
-        while x > place - 1:
+        while x >= place:
             ans *= 10
             ans += int(int(x % (place * 10)) / place)
             place *= 10
 
         if ans * sign < -2**31 or 2**31 - 1 < ans * sign:
             return 0
-            
+
         return ans * sign
